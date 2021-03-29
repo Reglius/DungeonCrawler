1e2e9132f79f96a6a4d1b267f7cf576311c6e030
Reglius <pawc3@cox.net>
2021-03-29 12:45:51 -0500

    Initial code for the project

---
+++ games/dungeon-crawler/scenes/main-scene.js
@@ -0,0 +1,26 @@
+export default {
+  name: "MainScene",
+  children: [
+    {
+      gameObject: {
+        name: "Ground",
+        components: [
+          { name: "DrawGeometryComponent", args: ["white"] },
+          { name: "RectangleGeometryComponent", args: [640, 480] },
+        ]
+      }, x: 320, y:340
+    },
+    {
+      prefabName: "MainController"
+    },
+    { prefabName: "Hero", x: 320, y: 340 },
+    { gameObject: { name: "ScreenTextShadow", components: [{ name: "ScreenTextComponent", args: ["DungeonCrawler", { color: "gray" }] }] }, x: 102, y: 42 },
+    { gameObject: { name: "ScreenText", components: [{ name: "ScreenTextComponent", args: ["DungeonCrawler", { color: "white" }] }] }, x: 100, y: 40 },
+  ],
+  mapLayout: [
+    [1, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]
+}

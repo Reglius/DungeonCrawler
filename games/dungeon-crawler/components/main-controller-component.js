75b59131b35d0c2a1d3831177ed4da71bb6106ad
Reglius <pawc3@cox.net>
2021-04-05 11:08:59 -0500

    changed width and height to be dymanic on scenes

--- games/dungeon-crawler/components/main-controller-component.js
+++ games/dungeon-crawler/components/main-controller-component.js
@@ -11,28 +11,34 @@ export default class MainControllerComponent extends Engine.Component {
     
   }
   update() {
+
+    let ground = SceneManager.currentScene.getGameObject("Ground");
+    let width = ground.getComponent("RectangleGeometryComponent").width;
+    let height = ground.getComponent("RectangleGeometryComponent").height;
+    // console.log("Ground x: " + ground.x + " y: " + ground.y + " width: " + width + " height: " + height);
+
     this.heroX = this.hero.x;
     this.heroY = this.hero.y;
-    if(this.heroX < 33){
+    if(this.heroX < ground.x - width/2){
       //Move left
       if(SceneManager.currentScene.name == "MainScene") return SceneManager.changeScene("RedScene")
       if(SceneManager.currentScene.name == "BlueScene") return SceneManager.changeScene("MainScene")
-      this.hero.x = 33;
+      this.hero.x += 10;
     }
-    if(this.heroX > 600){
+    if(this.heroX > ground.x + width/2){
       //Move right
       if(SceneManager.currentScene.name == "RedScene") return SceneManager.changeScene("MainScene")
       if(SceneManager.currentScene.name == "MainScene") return SceneManager.changeScene("BlueScene")
       
-      this.hero.x = 600
+      this.hero.x -= 10
     }
-    if(this.heroY < 100){
+    if(this.heroY < ground.y - height/2){
       //Move up
-      this.hero.y = 100;
+      this.hero.y += 10;
     }
-    if(this.heroY > 550){
+    if(this.heroY > ground.y + height/2){
       //Move Down
-      this.hero.y = 550
+      this.hero.y -= 10;
     }
   }
   
